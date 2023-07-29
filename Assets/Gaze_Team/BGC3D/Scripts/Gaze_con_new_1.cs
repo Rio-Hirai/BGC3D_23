c1427c3fcf90d93a4f4a71c17c6682b85e7df618
平井 李音（ヒライ リオ、Hirai Rio） <49777540+Rio-Hirai@users.noreply.github.com>
2023-07-30 02:16:11 +0900

    0730

--- Assets/Gaze_Team/BGC3D/Scripts/Gaze_con_new_1.cs
+++ Assets/Gaze_Team/BGC3D/Scripts/Gaze_con_new_1.cs
@@ -15,81 +15,63 @@ namespace ViveSR
 
             public class Gaze_con_new_1 : MonoBehaviour
             {
-                // サーバー接続
-                public GameObject Server;
-                private receiver script;
-
-                public float color_alpha = 0.45f;
+                public receiver script;                    // サーバと接続
+                public SRanipal_GazeRaySample rayset;      // レイキャストのデータを取得
 
                 [SerializeField]
-                private string tagName = "Enemy";        // インスペクターで変更可能
+                private string tagName = "Targets";          // 注視可能対象の選定．インスペクターで変更可能
 
-                public GameObject searchNearObj;         // 最も近いオブジェクト(public修飾子にすることで外部のクラスから参照できる)
-                public GameObject oldNearObj;
-                private float searchWaitTime = 0.02f;     // 検索の待機時間
+                public GameObject searchNearObj;           // 最も近いオブジェクト(public修飾子にすることで外部のクラスから参照できる)
+                public GameObject oldNearObj;              // 前フレームに注視していたターゲット
+                public Transform camera_obj;              // ユーザの位置
+                private float searchWaitTime = 1/60;      // 検索の待機時間
 
-                private float timer = 0f;                // 検索までの待機時間計測用
-
-                public SRanipal_GazeRaySample rayset;
-                public GameObject camera_obj;
-
-                private Vector3 cursor_point;
-                private float target_size;
-                private float distance_of_camera_to_target;
+                private float timer = 0f;                  // 検索までの待機時間計測用
+                private Vector3 cursor_point;               // カーソルの位置
+                private float target_size;                  // 注視しているターゲットの大きさ
+                private float distance_of_camera_to_target; // ユーザとターゲット間の距離
+                private float color_alpha = 0.45f;          // カーソルの透明度
 
                 void Start()
                 {
-                    // サーバと接続
-                    script = Server.GetComponent<receiver>();
-                    // 指定したタグを持つゲームオブジェクトのうち、このゲームオブジェクトに最も近いゲームオブジェクト１つを取得
-                    script = Server.GetComponent<receiver>();
-
-                    searchNearObj = Serch();
+                    searchNearObj = Serch();             // 注視ターゲット周りを初期化
+                    color_alpha = script.cursor_color.a; // カーソルの透明度を保存
                 }
 
                 void Update()
                 {
-                    // cursorの位置と半径と色を指定
-                    if (script.bubblegaze_switch)
+                    // カーソルの位置と半径と色を更新
+                    if (script.bubblegaze_switch) // カーソル表示・非常時スイッチの有無
                     {
-                        //script.cursor_color.a = color_alpha;
-                        this.GetComponent<Renderer>().material.color = script.cursor_color;
+                        this.GetComponent<Renderer>().material.color = script.cursor_color; // 透明度を0より大きくしてカーソルを表示
                     }
                     else
                     {
                         script.cursor_color.a = 0f;
-                        this.GetComponent<Renderer>().material.color = script.cursor_color;
+                        this.GetComponent<Renderer>().material.color = script.cursor_color; // 透明度を0にしてカーソルを非表示
                     }
-                    this.transform.position = cursor_point;
-                    this.transform.localScale = new Vector3(script.cursor_radious, script.cursor_radious, script.cursor_radious);
+                    this.transform.position = cursor_point; // カーソルの位置を更新
+                    this.transform.localScale = new Vector3(script.cursor_radious, script.cursor_radious, script.cursor_radious); // カーソルの大きさを更新
 
-                    // 時間の経過に合わせて自動的に取得する場合
-                    // 時間を計測
-                    timer += Time.deltaTime;
-                    // 検索の待機時間を経過したら
+                    timer += Time.deltaTime; // 時間を計測
+                    // 検索の待機時間を経過したら（処理が重くならないための処理なため場合によってはリファクタリング対象）
                     if (timer >= searchWaitTime)
                     {
-                        // 指定したタグを持つゲームオブジェクトのうち、このゲームオブジェクトに最も近いゲームオブジェクト１つを取得
-                        searchNearObj = Serch();
+                        searchNearObj = Serch(); // 指定したタグを持つオブジェクトのうち，このオブジェクトに最も近いゲームオブジェクトを取得
 
-                        // 計測時間を初期化して、再検索
-                        searchWaitTime = 0;
+                        timer = 0; // 計測時間を初期化して再検索
                     }
                 }
 
                 private GameObject Serch()
                 {
-                    // 最も近いオブジェクトの距離を代入するための変数
-                    float nearDistance = 0;
-                    // script.cursor_radious = nearDistance * 2 + script.target_size;
+                    float nearDistance = 0; // 最も近いオブジェクトの距離を代入するための変数
+                    float cursor_size_limit = distance_of_camera_to_target / 8; // カーソルの大きさの上限を設定するための変数（でないと無限に大きくなる）
+                    GameObject searchTargetObj = null; // 検索された最も近いゲームオブジェクトを代入するための変数
 
-                    // 検索された最も近いゲームオブジェクトを代入するための変数
-                    GameObject searchTargetObj = null;
+                    GameObject[] objs = GameObject.FindGameObjectsWithTag(tagName); // tagNameで指定されたTagを持つ、すべてのゲームオブジェクトを配列に取得
 
-                    // tagNameで指定されたTagを持つ、すべてのゲームオブジェクトを配列に取得
-                    GameObject[] objs = GameObject.FindGameObjectsWithTag(tagName);
-
-                    // 取得したゲームオブジェクトが 0 ならnullを戻す(使用する場合にはnullでもエラーにならない処理にしておくこと)
+                    // 取得したゲームオブジェクトが0ならnullを返す(nullでもエラーにならないように処理)
                     if (objs.Length == 0)
                     {
                         return searchTargetObj;
@@ -98,42 +80,31 @@ namespace ViveSR
                     // ここの条件を確認する！！（リファクタリング部分）
                     if (script.BlinkCount > -1)
                     {
-                        // objsから１つずつobj変数に取り出す
-                        foreach (GameObject obj in objs)
+                        foreach (GameObject obj in objs) // objsから1つずつobjに取り出す
                         {
-                            // 直線の始点からオブジェクトまでのベクトルを計算します
-                            Vector3 toObject = obj.transform.position - rayset.ray0;
-
-                            // 直線に対するオブジェクトの投影点を計算します
-                            Vector3 projection = Vector3.Project(toObject, rayset.ray1.normalized);
-
-                            // 投影点を元に直線上の最も近い点を計算します
-                            cursor_point = rayset.ray0 + projection;
-
-                            // ターゲットと直線上の最も近い点との距離を計算します
-                            float distance = Vector3.Distance(obj.transform.position, cursor_point);
+                            Vector3 toObject = obj.transform.position - rayset.ray0; // 直線の始点からオブジェクトまでのベクトルを計算
+                            Vector3 projection = Vector3.Project(toObject, rayset.ray1.normalized); // 直線に対するオブジェクトの投影点を計算
+                            cursor_point = rayset.ray0 + projection; // 投影点を元に直線上の最も近い点を計算
+                            float distance = Vector3.Distance(obj.transform.position, cursor_point); // ターゲットと直線上の最も近い点との距離を計算
 
                             // nearDistanceが0(最初はこちら)、あるいはnearDistanceがdistanceよりも大きい値なら
                             if (nearDistance == 0 || nearDistance > distance)
                             {
-                                // nearDistanceを更新
-                                nearDistance = distance;
+                                nearDistance = distance; // nearDistanceを更新
+                                searchTargetObj = obj; // searchTargetObjを更新
+                                target_size = obj.transform.localScale.x; // 注視しているターゲットの大きさを更新．ターゲットの大きさが全次元で一律のためx次元のみ取得
+                                distance_of_camera_to_target = Vector3.Distance(camera_obj.position, obj.transform.position);
 
-                                // searchTargetObjを更新
-                                searchTargetObj = obj;
-                                target_size = obj.transform.localScale.x;
-                                distance_of_camera_to_target = Vector3.Distance(camera_obj.transform.position, obj.transform.position);
-
-                                // 
-                                if (nearDistance < (distance_of_camera_to_target / 8)) // オブジェクト間の距離が一定未満の場合
+                                // カーソルの大きさの上限に抵触した場合の処理
+                                if (nearDistance < (cursor_size_limit)) // オブジェクト間の距離が一定未満＝カーソルの大きさが最大未満の場合
                                 {
-                                    script.DwellTarget = searchTargetObj;
-                                    script.cursor_color.a = color_alpha;
+                                    script.cursor_color.a = color_alpha; // カーソルの透明度を調整して表示
+                                    script.DwellTarget = searchTargetObj; // 注視しているオブジェクトを更新
                                 }
                                 else
                                 {
-                                    script.DwellTarget = null;
-                                    script.cursor_color.a = 0;
+                                    script.cursor_color.a = 0; // カーソルの透明度を調整して非表示
+                                    script.DwellTarget = null; // 注視しているオブジェクトを更新
                                 }
                             }
                         }
@@ -141,36 +112,28 @@ namespace ViveSR
 
                     // 最も近かったオブジェクトを返す
                     // 注視していたターゲットが変わった（連続注視が途切れた）場合
-                    if (oldNearObj != searchTargetObj || nearDistance > (distance_of_camera_to_target / 8))
+                    if (oldNearObj != searchTargetObj || nearDistance > (cursor_size_limit))
                     {
                         script.same_target = false;
-                        script.select_target_id = -1;
-                        searchTargetObj.GetComponent<target_para_set>().dtime = 0;
-                        script.BlinkFlag = 0;
+                        script.select_target_id = -1; // 選択状態のターゲットを初期化
+                        searchTargetObj.GetComponent<target_para_set>().dtime = 0; // 累計注視時間を初期化
 
-                        if (nearDistance * 2 + target_size < (distance_of_camera_to_target / 8))
-                        {
-                            script.lens_flag = true;
-                        }
+                        script.BlinkFlag = 0; // 連続瞬きを初期化
                     }
 
-                    // 時間を計測
-                    searchTargetObj.GetComponent<target_para_set>().dtime += Time.deltaTime;
+                    searchTargetObj.GetComponent<target_para_set>().dtime += Time.deltaTime; // 注視しているターゲットの累計注視時間を追加
 
                     // 一定時間注視していた場合
-                    if (((searchTargetObj.GetComponent<target_para_set>().dtime >= script.set_dtime || script.BlinkFlag > 0) && nearDistance * 2 + script.target_size < (script.Depth / 8)) || script.next_step__flag)
+                    if (((searchTargetObj.GetComponent<target_para_set>().dtime >= script.set_dtime || script.BlinkFlag > 0) && nearDistance * 2 + target_size < (cursor_size_limit)) || script.next_step__flag)
                     {
-                        script.select_target_id = searchTargetObj.GetComponent<target_para_set>().Id;
-                        script.next_step__flag = false;
+                        script.select_target_id = searchTargetObj.GetComponent<target_para_set>().Id; // 選択したターゲットのIDを更新（このIDが結果として出力される）
+                        script.next_step__flag = false; // タスク間の休憩状態に遷移するためのフラグを更新
                     }
 
-                    // 注視しているターゲットを更新
-                    oldNearObj = searchTargetObj;
+                    oldNearObj = searchTargetObj; // 注視しているターゲットを更新
+                    script.cursor_radious = nearDistance * 2 + target_size; // カーソルの大きさを更新
 
-                    script.cursor_radious = nearDistance * 2 + target_size;
-
-                    // 最も近いオブジェクトを返す
-                    return searchTargetObj;
+                    return searchTargetObj; // 最も近いオブジェクトを返す
                 }
             }
         }
