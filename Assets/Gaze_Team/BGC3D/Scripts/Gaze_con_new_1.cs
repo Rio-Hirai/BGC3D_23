5a382c70474a9a2d3c6e8d6af149746be69c5e8d
平井 李音（ヒライ リオ、Hirai Rio） <49777540+Rio-Hirai@users.noreply.github.com>
2023-08-06 23:59:15 +0900

    0806
    Gaze_con_new_1.csをリファクタリング

--- Assets/Gaze_Team/BGC3D/Scripts/Gaze_con_new_1.cs
+++ Assets/Gaze_Team/BGC3D/Scripts/Gaze_con_new_1.cs
@@ -49,7 +49,7 @@ namespace ViveSR
                     }
                     else
                     {
-                        script.cursor_color.a = 0f;
+                        script.cursor_color.a = 0f; // カーソルを透明化（＝非表示化）
                         this.GetComponent<Renderer>().material.color = script.cursor_color; // 透明度を0にしてカーソルを非表示
                     }
                     this.transform.position = cursor_point; // カーソルの位置を更新
@@ -71,12 +71,13 @@ namespace ViveSR
                     float cursor_size_limit = distance_of_camera_to_target / 8; // カーソルの大きさの上限を設定するための変数（でないと無限に大きくなる）
                     GameObject searchTargetObj = null; // 検索された最も近いゲームオブジェクトを代入するための変数
                     script.cursor_count = 0; // バブルカーソル内に存在するターゲットの数を初期化
-                    Vector3 ray1 = rayset.ray1;
+                    Vector3 ray1 = rayset.ray1; // 視線の方向ベクトルを保存（動的移動平均フィルタを用いるため）
                     GameObject[] objs = GameObject.FindGameObjectsWithTag(tagName); // tagNameで指定されたTagを持つ、すべてのゲームオブジェクトを配列に取得
 
                     if (objs.Length == 0) return searchTargetObj; // 取得したゲームオブジェクトが0ならnullを返す(nullでもエラーにならないように処理)
 
-                    if (script.MAverageFilter)
+                    // 移動平均フィルタの処理
+                    if (script.MAverageFilter) // 移動平均フィルタの機能がオンの場合
                     {
                         foreach (GameObject obj in objs) // objsから1つずつobjに取り出す
                         {
@@ -88,8 +89,10 @@ namespace ViveSR
                             if (distance < cursor_size_limit / 2) script.cursor_count++; // カーソル内に存在するターゲットをカウント
                         }
 
-                        ray1 = filter.filter(rayset.ray1, script.cursor_count); // 動的移動平均フィルタを適用（第一引数がフィルタリングする値，第二引数が窓の大きさ）
+                        ray1 = filter.filter(rayset.ray1, script.cursor_count); // 視線の方向ベクトルに動的移動平均フィルタを適用（第一引数がフィルタリングする値，第二引数が窓の大きさ）
                     }
+                    //--------------------------------------------------------------
+
 
                     // ここの条件を確認する！！（リファクタリング部分）
                     if (script.BlinkCount > -1)
@@ -101,6 +104,7 @@ namespace ViveSR
                             cursor_point = rayset.ray0 + projection; // 投影点を元に直線上の最も近い点を計算
                             float distance = Vector3.Distance(obj.transform.position, cursor_point); // ターゲットと直線上の最も近い点との距離を計算
 
+
                             // nearDistanceが0(最初はこちら)、あるいはnearDistanceがdistanceよりも大きい値なら
                             if (nearDistance == 0 || nearDistance > distance)
                             {
@@ -109,7 +113,7 @@ namespace ViveSR
                                 target_size = obj.transform.localScale.x; // 注視しているターゲットの大きさを更新．ターゲットの大きさが全次元で一律のためx次元のみ取得
                                 distance_of_camera_to_target = Vector3.Distance(camera_obj.position, obj.transform.position); // ユーザとターゲット間の距離を更新
 
-                                // カーソルの大きさの上限に抵触した場合の処理
+                                // カーソルの大きさの上限に抵触した場合の処理-------------------
                                 if (nearDistance < (cursor_size_limit)) // オブジェクト間の距離が一定未満＝カーソルの大きさが最大未満の場合
                                 {
                                     script.cursor_color.a = color_alpha; // カーソルの透明度を調整して表示
@@ -120,12 +124,15 @@ namespace ViveSR
                                     script.cursor_color.a = 0; // カーソルの透明度を調整して非表示
                                     script.DwellTarget = null; // 注視しているオブジェクトを更新
                                 }
+                                //--------------------------------------------------------------
                             }
+                            //--------------------------------------------------------------
                         }
                     }
 
+
                     // 最も近かったオブジェクトを返す
-                    // 注視していたターゲットが変わった（連続注視が途切れた）場合
+                    // 注視していたターゲットが変わった（連続注視が途切れた）場合---
                     if (oldNearObj != searchTargetObj || nearDistance > cursor_size_limit)
                     {
                         script.same_target = false;
@@ -134,15 +141,20 @@ namespace ViveSR
 
                         script.BlinkFlag = 0; // 連続瞬きを初期化
                     }
+                    //--------------------------------------------------------------
+
 
                     searchTargetObj.GetComponent<target_para_set>().dtime += Time.deltaTime; // 注視しているターゲットの累計注視時間を追加
 
-                    // 一定時間注視していた場合
+
+                    // 一定時間注視していた場合--------------------------------------
                     if (((searchTargetObj.GetComponent<target_para_set>().dtime >= script.set_dtime || script.BlinkFlag > 0) && nearDistance * 2 + target_size < (cursor_size_limit)) || script.next_step__flag)
                     {
                         script.select_target_id = searchTargetObj.GetComponent<target_para_set>().Id; // 選択したターゲットのIDを更新（このIDが結果として出力される）
                         script.next_step__flag = false; // タスク間の休憩状態に遷移するためのフラグを更新
                     }
+                    //--------------------------------------------------------------
+
 
                     oldNearObj = searchTargetObj; // 注視しているターゲットを更新
                     script.cursor_radious = nearDistance * 2 + target_size; // カーソルの大きさを更新
