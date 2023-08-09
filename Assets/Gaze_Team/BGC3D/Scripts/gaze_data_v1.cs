85c966315ff2b9a3e6b7187593c18107403f7247
平井 李音（ヒライ リオ、Hirai Rio） <hirai@iplab.cs.tsukuba.ac.jp>
2023-08-09 11:53:26 +0900

    gaze_data_v1.csからeye_callbackを分離

--- Assets/Gaze_Team/BGC3D/Scripts/gaze_data_v1.cs
+++ Assets/Gaze_Team/BGC3D/Scripts/gaze_data_v1.cs
@@ -36,19 +36,19 @@ namespace ViveSR
 
                 private void Update()
                 {
-                    if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
-                        SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT) return;
+                    //if (SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.WORKING &&
+                    //    SRanipal_Eye_Framework.Status != SRanipal_Eye_Framework.FrameworkStatus.NOT_SUPPORT) return;
 
-                    if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
-                    {
-                        SRanipal_Eye.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
-                        eye_callback_registered = true;
-                    }
-                    else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
-                    {
-                        SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
-                        eye_callback_registered = false;
-                    }
+                    //if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == true && eye_callback_registered == false)
+                    //{
+                    //    SRanipal_Eye.WrapperRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+                    //    eye_callback_registered = true;
+                    //}
+                    //else if (SRanipal_Eye_Framework.Instance.EnableEyeDataCallback == false && eye_callback_registered == true)
+                    //{
+                    //    SRanipal_Eye.WrapperUnRegisterEyeDataCallback(Marshal.GetFunctionPointerForDelegate((SRanipal_Eye.CallbackBasic)EyeCallback));
+                    //    eye_callback_registered = false;
+                    //}
 
                     //// ·ŸÇÃäJÇ´ãÔçá
                     //float leftopeness, rightopness;
@@ -247,7 +247,6 @@ namespace ViveSR
                 private static void EyeCallback(ref EyeData eye_data)
                 {
                     eyeData = eye_data;
-                    // Debug.Log("OK = " + eyeData.verbose_data.right.eye_openness);
                 }
             }
         }
