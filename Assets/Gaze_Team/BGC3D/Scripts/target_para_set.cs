e644c422012ccdcdc37fa7bec734df75e258d2c7
平井 李音（ヒライ リオ、Hirai Rio） <49777540+Rio-Hirai@users.noreply.github.com>
2023-08-12 21:14:58 +0900

    target_para_setをリファクタリング

--- Assets/Gaze_Team/BGC3D/Scripts/target_para_set.cs
+++ Assets/Gaze_Team/BGC3D/Scripts/target_para_set.cs
@@ -25,7 +25,7 @@ public class target_para_set : MonoBehaviour
     {
         if (Server.GetComponent<receiver>().enabled == false)
         {
-            // 結果表示用の処理
+            // 結果表示用の処理---------------------------------------------
             for (int i = 1; i < script2.csvDatas.Count; i++)
             {
                 if (Id == int.Parse(script2.csvDatas[i][2]))
@@ -33,123 +33,125 @@ public class target_para_set : MonoBehaviour
                     this.GetComponent<Renderer>().material.color = Color.blue;
                 }
             }
+            //--------------------------------------------------------------
 
             return;
         }
 
 
-        // 累積注視時間モード時の処理
-        if (script.total_DwellTime_mode)
+        // 累積注視時間モード時の処理----------------------------------
+        if (script.total_DwellTime_mode) // 累積注視時間モードがオンの時
         {
-            if (script.DwellTarget != this.gameObject)
+            if (script.DwellTarget != this.gameObject) // このターゲットと注視中のターゲットが異なる場合
             {
-                if (dtime > 0)
+                if (dtime > 0) // 累計注視時間が0より大きい場合
                 {
-                    dtime -= Time.deltaTime;
+                    dtime -= Time.deltaTime; // 累計注視時間を非注視時間分減らす
                 }
                 else
                 {
-                    dtime = 0;
+                    dtime = 0; // 累計注視時間を0にする
                 }
             }
 
-            if(Id != 999 && script.taskflag == false)
+            if(Id != 999 && script.taskflag == false) // IDが999（＝黒いターゲット）かつ非タスク状態の場合
             {
-                dtime = 0;
+                dtime = 0; // 累計注視時間を0にする．この処理のせいで非タスク中に練習を行えないため，要改良部分
             }
 
-            if (dtime >= 1)
+            if (dtime >= script.set_dtime) // 累計注視時間が設定した時間以上の場合，
             {
-                script.select_target_id = Id;
-                script.selecting_target = this.gameObject;
-                script.same_target = false;
+                script.selecting_target = this.gameObject; // 選択されたターゲットを更新
+                script.select_target_id = Id; // 選択されたターゲットのIDを更新
+                script.same_target = false; // ？？？
             }
         }
+        //--------------------------------------------------------------
 
 
-        // 色変化の処理（要リファクタリング部分）
+        // 色変化の処理（要リファクタリング部分）-----------------------
         if (script.controller_switch)
         {
-            if (script.output_flag)
+            if (script.output_flag) // セッション終了時
             {
                 this.GetComponent<Renderer>().material.color = script.target_color;
             }
-            else if (Id == script.tasknums[script.task_num] && script.taskflag)
+            else if (Id == script.tasknums[script.task_num] && script.taskflag) // 提示したターゲットとIDが同じ，かつタスク中の場合
             {
-                this.GetComponent<Renderer>().material.color = Color.blue;
+                this.GetComponent<Renderer>().material.color = Color.blue; // ターゲットを青色に変更
             }
-            else if (Id == 999)
+            else if (Id == 999) // ターゲットが黒色のIDを持つ場合
             {
-                this.GetComponent<Renderer>().material.color = Color.black;
+                this.GetComponent<Renderer>().material.color = Color.black; // ターゲットを黒色に変更
             }
-            else if (script.DwellTarget != null)
+            else if (script.DwellTarget != null) // 注視中のターゲットが無い場合
             {
-                if (script.DwellTarget.GetComponent<target_para_set>().Id == Id)
+                if (script.DwellTarget.GetComponent<target_para_set>().Id == Id) // 注視中のターゲットのIDを更新
                 {
-                    this.GetComponent<Renderer>().material.color = script.select_color;
+                    this.GetComponent<Renderer>().material.color = script.select_color; // ターゲットの色を変更
                 }
             }
             else
             {
-                this.GetComponent<Renderer>().material.color = Color.white;
+                this.GetComponent<Renderer>().material.color = Color.white; // ターゲットを白色に変更
             }
         }
         else
         {
-            if (script.output_flag)
+            if (script.output_flag) //
             {
-                this.GetComponent<Renderer>().material.color = script.target_color;
+                this.GetComponent<Renderer>().material.color = script.target_color; //
             }
-            else if (Id == script.select_target_id)
+            else if (Id == script.select_target_id) //
             {
-                this.GetComponent<Renderer>().material.color = script.target_color;
+                this.GetComponent<Renderer>().material.color = script.target_color; //
             }
-            else if (script.DwellTarget == null)
+            else if (script.DwellTarget == null) //
             {
-                if (script.taskflag == false && Id != 999)
+                if (script.taskflag == false && Id != 999) //
                 {
-                    this.GetComponent<Renderer>().material.color = Color.white;
+                    this.GetComponent<Renderer>().material.color = Color.white; //
                 }
-                else if (Id == script.select_target_id)
+                else if (Id == script.select_target_id) //
                 {
-                    this.GetComponent<Renderer>().material.color = script.target_color;
+                    this.GetComponent<Renderer>().material.color = script.target_color; //
                 }
-                else if (Id == script.tasknums[script.task_num])
+                else if (Id == script.tasknums[script.task_num]) //
                 {
-                    this.GetComponent<Renderer>().material.color = Color.blue;
+                    this.GetComponent<Renderer>().material.color = Color.blue; //
                 }
-                else if (Id == 999)
+                else if (Id == 999) //
                 {
-                    this.GetComponent<Renderer>().material.color = Color.black;
+                    this.GetComponent<Renderer>().material.color = Color.black; //
                 }
                 else
                 {
-                    this.GetComponent<Renderer>().material.color = Color.white;
+                    this.GetComponent<Renderer>().material.color = Color.white; //
                 }
             }
-            else if (script.DwellTarget.GetComponent<target_para_set>().Id == Id)
+            else if (script.DwellTarget.GetComponent<target_para_set>().Id == Id) //
             {
-                this.GetComponent<Renderer>().material.color = script.select_color;
+                this.GetComponent<Renderer>().material.color = script.select_color; //
             }
-            else if (script.taskflag == false && Id != 999)
+            else if (script.taskflag == false && Id != 999) //
             {
-                this.GetComponent<Renderer>().material.color = Color.white;
+                this.GetComponent<Renderer>().material.color = Color.white; //
             }
-            else if (Id == script.select_target_id)
+            else if (Id == script.select_target_id) //
             {
-                this.GetComponent<Renderer>().material.color = script.target_color;
+                this.GetComponent<Renderer>().material.color = script.target_color; //
             }
-            else if (Id == script.tasknums[script.task_num])
+            else if (Id == script.tasknums[script.task_num]) //
             {
-                this.GetComponent<Renderer>().material.color = Color.blue;
+                this.GetComponent<Renderer>().material.color = Color.blue; //
             }
-            else if (Id == 999)
+            else if (Id == 999) //
             {
-                this.GetComponent<Renderer>().material.color = Color.black;
+                this.GetComponent<Renderer>().material.color = Color.black; //
             }
             else
             {
-                this.GetComponent<Renderer>().material.color = Color.white;
+                this.GetComponent<Renderer>().material.color = Color.white; //
             }
         }
     }
