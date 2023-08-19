e125cd218a1a08187e26fb0e57e12757218b9749
平井 李音（ヒライ リオ、Hirai Rio） <49777540+Rio-Hirai@users.noreply.github.com>
2023-08-19 23:40:17 +0900

    コメントを追加

--- Assets/Gaze_Team/BGC3D/Scripts/target_size_set.cs
+++ Assets/Gaze_Team/BGC3D/Scripts/target_size_set.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class target_size_set : MonoBehaviour
 {
-    public receiver Server;
-    public float dtime;
+    public receiver Server; // サーバ接続
+    public float dtime; // 累計注視時間を格納する変数
 
     void Start()
     {
-        this.transform.localScale = new Vector3(Server.target_size, Server.target_size, Server.target_size);
-        this.name = "target_" + Server.target_id;
-        this.GetComponent<target_para_set>().Id = Server.target_id;
-        Server.target_id++;
+        this.transform.localScale = new Vector3(Server.target_size, Server.target_size, Server.target_size); // ターゲットの大きさを初期化
+        this.name = "target_" + Server.target_id; // ターゲットの名前を初期化
+        this.GetComponent<target_para_set>().Id = Server.target_id; // ターゲットのIDを初期化
+        Server.target_id++; // ターゲットのIDを連番にするために加算
     }
 }
