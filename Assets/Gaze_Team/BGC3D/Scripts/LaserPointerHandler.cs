8136f87ea093ea6f22e0ee44fbb013dd17dacf42
平井 李音（ヒライ リオ、Hirai Rio） <49777540+Rio-Hirai@users.noreply.github.com>
2023-08-21 23:58:16 +0900

    リファクタリング

--- Assets/Gaze_Team/BGC3D/Scripts/LaserPointerHandler.cs
+++ Assets/Gaze_Team/BGC3D/Scripts/LaserPointerHandler.cs
@@ -9,7 +9,7 @@ public class LaserPointerHandler : MonoBehaviour
 {
     public SteamVR_LaserPointer laserPointer;   // 右コントローラのレイ
     public SteamVR_LaserPointer laserPointer2;  // 左コントローラのレイ
-    public receiver Server;                     // サーバ
+    public receiver Server;                     // サーバと接続
 
     void Awake()
     {
@@ -31,21 +31,21 @@ public class LaserPointerHandler : MonoBehaviour
     {
         if(Server.laserswitch)
         {
-            GameObject testcube = GameObject.Find(e.target.name);
-            Server.same_target = false;
-            testcube.GetComponent<Renderer>().material.color = Server.target_color;
-            Debug.Log("OK = ");
+            GameObject testcube = GameObject.Find(e.target.name); // 焦点を合わせたターゲットを取得
+            Server.same_target = false; // ？？？
+            testcube.GetComponent<Renderer>().material.color = Server.target_color; // 焦点を合わせたターゲットの色を変更
 
-            if (testcube.tag == "Targets")
+
+            // タグに応じた処理---------------------------------------------
+            if (testcube.tag == "Targets") // 焦点を合わせたターゲットのタグが「Targets」の場合
             {
-                Server.select_target_id = testcube.GetComponent<target_para_set>().Id;
-                Debug.Log("OK_id");
+                Server.select_target_id = testcube.GetComponent<target_para_set>().Id; // 選択されたターゲットのIDを更新
             }
-            else if (testcube.tag == "UI")
+            else if (testcube.tag == "UI") // 焦点を合わせたターゲットのタグが「UI」の場合
             {
-                testcube.GetComponent<UI_default>().Click_flag = true;
-                Debug.Log("OK_ui");
+                testcube.GetComponent<UI_default>().Click_flag = true; // 選択されたUIのクリックフラグをTrueにする
             }
+            //--------------------------------------------------------------
         }
     }
     //--------------------------------------------------------------
@@ -54,8 +54,8 @@ public class LaserPointerHandler : MonoBehaviour
     // レーザーポインターがtargetに触れたとき-----------------------
     public void PointerInside(object sender, PointerEventArgs e)
     {
-        GameObject testcube = GameObject.Find(e.target.name);
-        Server.DwellTarget = testcube;
+        GameObject testcube = GameObject.Find(e.target.name); // ？？？
+        Server.DwellTarget = testcube; // ？？？
     }
     //--------------------------------------------------------------
 
@@ -63,11 +63,11 @@ public class LaserPointerHandler : MonoBehaviour
     // レーザーポインターがtargetから離れたとき---------------------
     public void PointerOutside(object sender, PointerEventArgs e)
     {
-        GameObject testcube = GameObject.Find(e.target.name);
-        Server.DwellTarget = null;
-        Server.select_target_id = -1;
-        Server.selecting_target = null;
-        testcube.GetComponent<Renderer>().material.color = Color.white;
+        GameObject testcube = GameObject.Find(e.target.name); // ？？？
+        Server.DwellTarget = null; // ？？？
+        Server.select_target_id = -1; // ？？？
+        Server.selecting_target = null; // ？？？
+        testcube.GetComponent<Renderer>().material.color = Color.white; // ？？？
     }
     //--------------------------------------------------------------
 }
