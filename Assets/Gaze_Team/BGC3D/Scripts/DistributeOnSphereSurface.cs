e655c58601c951000642cfd4a90fb18f23991205
平井 李音（ヒライ リオ、Hirai Rio） <49777540+Rio-Hirai@users.noreply.github.com>
2023-09-21 23:00:00 +0900

    リファクタリング

--- Assets/Gaze_Team/BGC3D/Scripts/DistributeOnSphereSurface.cs
+++ Assets/Gaze_Team/BGC3D/Scripts/DistributeOnSphereSurface.cs
@@ -4,8 +4,8 @@ public class DistributeOnSphereSurface : MonoBehaviour
 {
     public GameObject[] objectsToDistribute; // 配置するオブジェクトの配列
     public GameObject baseSphere; // Base_Sphereオブジェクト
-    public float targetSize = 0.25f; // ターゲットのサイズ
-    public float spacing = 0.01f; // ターゲット間の間隔
+    public float targetSize = 0.1f; // ターゲットのサイズ
+    public float spacing = 0.12f; // ターゲット間の間隔
 
     void Start()
     {
@@ -14,13 +14,13 @@ public class DistributeOnSphereSurface : MonoBehaviour
 
     void DistributeObjects()
     {
-        if (objectsToDistribute.Length != 25) // 5x5のグリッドのため、25のオブジェクトが必要
+        if (objectsToDistribute.Length != 49) // 7x7のグリッドのため、49のオブジェクトが必要
         {
-            Debug.LogError("The number of objects to distribute should be 25.");
+            Debug.LogError("The number of objects to distribute should be 49.");
             return;
         }
 
-        float sphereRadius = baseSphere.transform.localScale.x * 0.5f; // Base_Sphereの半径を取得
+        float sphereRadius = 3.5f; // Base_Sphereの半径を取得
         Vector3 centerPosition = transform.position + transform.forward * sphereRadius;
 
         Vector3 forward = transform.forward;
@@ -29,11 +29,11 @@ public class DistributeOnSphereSurface : MonoBehaviour
 
         Vector3 start = centerPosition - 2 * (targetSize + spacing) * right - 2 * (targetSize + spacing) * up;
 
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 7; i++)
         {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < 7; j++)
             {
-                int index = i * 5 + j;
+                int index = i * 7 + j;
                 Vector3 offset = j * (targetSize + spacing) * right + i * (targetSize + spacing) * up;
                 Vector3 targetPosition = start + offset;
 
